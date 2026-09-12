5fe28add95bf6d8d96fc28e5139a7179f12000de
21004 <2100499457@qq.com>
2026-09-12 09:07:40 +0800

    使子线程遇错时主线程及时结束

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import logging
+import sys
+import threading
+import traceback
+
 import colorlog
 import os
 
 os.environ["PYCRAFT_CLIENT"] = "1"
 
 from src.client import client_main
+
+def thread_excepthook(args):
+    print(f"An runtime error occurred in {args.thread.name}.", file=sys.stderr)
+    traceback.print_exception(
+        args.exc_type,
+        args.exc_value,
+        args.exc_traceback
+    )
+    if client is not None:
+        logging.info("Force shutdown client")
+        client.shutdown()
+    os._exit(1)
 
 if __name__ == "__main__":
     handler = colorlog.StreamHandler()
@@ -25,6 +41,8 @@ if __name__ == "__main__":
     logger = logging.getLogger()
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
+
+    threading.excepthook = thread_excepthook
 
     client = client_main.Client()
     logging.info("Client started")
